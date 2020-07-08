efb8cea1a55298e39125fb4763b5959a771ad7a2
vasily-esipenko <esipenkovasilij@gmail.com>
2020-07-08 13:28:49 +0300

    Update parse.py: add lxml parsing

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
-import os.path
 from bs4 import BeautifulSoup as bs
 import requests as r
 from urllib.parse import urlparse
+import lxml
 
 usd = "https://www.google.com/search?q=dollar+to+ruble&oq=doll&aqs=chrome.1.69i59l3j0l3j46j0.1757j0j7&sourceid=chrome&ie=UTF-8"
 eur = "https://www.google.com/search?q=eur+to+ruble&oq=eur+&aqs=chrome.0.69i59j69i57j0l6.1302j1j7&sourceid=chrome&ie=UTF-8"
@@ -10,6 +10,6 @@ btc = "https://www.google.com/search?q=btc+to+dollar&oq=btc&aqs=chrome.0.69i59j6
 
 def currency(ccy):
     url = r.get(ccy)
-    soup = bs(url.content, "html.parser")
+    soup = bs(url.content, "lxml")
     res = soup.find_all("div", class_="BNeawe iBp4i AP7Wnd")
     return str(res[1])
